113525d110e3defcd6677d8bd9162627bceee291
RafalToJa <73687172+RafalToJa@users.noreply.github.com>
2021-06-23 21:01:58 +0200

    Add files via upload

---
+++ Factories.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Factories : MonoBehaviour
+{
+    public int team;
+    public int Turn;
+    public bool selected;
+
+    GameMaster gm;
+
+    public GameObject FactoryUI;
+    public static bool FactoryUIOpen = false;
+
+    public GameObject miecznikPrefab;
+    public Button Miecznik;
+    public int miecznikCost;
+
+    public GameObject tarczownikPrefab;
+    public Button Tarczownik;
+    public int tarczownikCost;
+
+    public GameObject kuszniczkaPrefab;
+    public Button Kuszniczka;
+    public int kuszniczkaCost;
+
+    private int money1;
+    private int money2;
+
+    private void Start()
+    {
+        Turn = 1;
+        gm = FindObjectOfType<GameMaster>();
+
+        Button miecz = Miecznik.GetComponent<Button>();
+        miecz.onClick.AddListener(miecznikCreation);
+
+        Button tarcz = Tarczownik.GetComponent<Button>();
+        tarcz.onClick.AddListener(tarczownikCreation);
+
+        Button kusz = Kuszniczka.GetComponent<Button>();
+        kusz.onClick.AddListener(kuszniczkaCreation);
+    }
+
+    void Update()
+    { 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Turn += 1;
+        }
+
+        //money1 = GetComponent<Mines>().player1Money;
+        //money2 = GetComponent<Mines>().player2Money;
+        //EndTurn();
+        /*if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (FactoryUIOpen)
+            {
+                Close();
+            }
+        }*/
+    }
+    /*private void EndTurn()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // Zmienic na przycisk
+        {
+            Turn += 1;
+        }
+    }*/
+    void Close()
+    {
+        FactoryUI.SetActive(false);
+        FactoryUIOpen = false;
+    }
+
+    void Open()
+    {
+        FactoryUI.SetActive(true);
+        FactoryUIOpen = true;
+    }
+    
+    
+    
+    private void OnMouseDown()
+    {
+        if (Turn % 2 == 1 && team == 1)
+        {
+
+
+            if (selected == true)
+            {
+                selected = false;
+                gm.selectedFactory = null;
+            }
+            else
+            {
+
+
+                if (gm.selectedFactory != null)
+                {
+                    gm.selectedFactory.selected = false;
+                }
+
+                selected = true;
+                gm.selectedFactory = this;
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    if (FactoryUIOpen)
+                    {
+                        Close();
+                    }
+                    else
+                    {
+                        Open();
+                    }
+                }
+
+            }
+        }
+        if (Turn % 2 == 0 && team == 2)
+        {
+
+
+            if (selected == true)
+            {
+                selected = false;
+                gm.selectedFactory = null;
+            }
+            else
+            {
+
+
+                if (gm.selectedFactory != null)
+                {
+                    gm.selectedFactory.selected = false;
+                }
+
+                selected = true;
+                gm.selectedFactory = this;
+                if(Input.GetKeyDown(KeyCode.F))
+                {
+                    if (FactoryUIOpen)
+                    {
+                        Close();
+                    }
+                    else
+                    {
+                        Open();
+                    }
+                }
+
+            }
+        }
+    }
+
+    private void miecznikCreation()
+    {
+        if(team == 1)
+        {
+            money1 = GetComponent<Mines>().player1Money;
+            miecznikPrefab.GetComponent<Units>().Player = 1;
+            Instantiate(miecznikPrefab);
+            money1 -= miecznikCost;
+            
+        }
+        if (team == 2)
+        {
+            money2 = GetComponent<Mines>().player2Money;
+            miecznikPrefab.GetComponent<Units>().Player = 2;
+            Instantiate(miecznikPrefab);
+            money2 -= miecznikCost;            
+        }
+    }
+    private void tarczownikCreation()
+    {
+        if (team == 1)
+        {
+            money1 = GetComponent<Mines>().player1Money;
+            tarczownikPrefab.GetComponent<Units>().Player = 1;
+            Instantiate(tarczownikPrefab);
+            money1 -= tarczownikCost;
+        }
+        if (team == 2)
+        {
+            money2 = GetComponent<Mines>().player2Money;
+            tarczownikPrefab.GetComponent<Units>().Player = 2;
+            Instantiate(tarczownikPrefab);
+            money2 -= tarczownikCost;
+        }
+    }
+    private void kuszniczkaCreation()
+    {
+        if (team == 1)
+        {
+            money1 = GetComponent<Mines>().player1Money;
+            kuszniczkaPrefab.GetComponent<Units>().Player = 1;
+            Instantiate(kuszniczkaPrefab);
+            money1 -= kuszniczkaCost;
+        }
+        if (team == 2)
+        {
+            money2 = GetComponent<Mines>().player2Money;
+            kuszniczkaPrefab.GetComponent<Units>().Player = 2;
+            Instantiate(kuszniczkaPrefab);
+            money2 -= kuszniczkaCost;
+        }
+    }
+
+}
---
+++ Units.cs
@@ -0,0 +1,256 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Units : MonoBehaviour
+{
+    public bool selected;
+    GameMaster gm;
+    public List<Tile> tiles;
+    public double tileSpeed;
+    public float attackRange;
+    public bool hasMoved;
+    public bool hasAttacked;
+    public int Player;
+    public int Turn;
+    public int Health;
+    public int defence;
+    public int curHealth;
+    public int attackDamage;
+    public Transform attackPoint;
+    public bool attackable;
+
+
+    public float moveSpeed;
+
+    private void Start()
+    {
+        defence = defence / 100;
+        Turn = 1;
+        gm = FindObjectOfType<GameMaster>();
+        curHealth = Health;
+    }
+
+    private void OnMouseDown()
+    {
+
+        if (Turn % 2 == 1 && Player == 1)
+        {
+
+
+            if (selected == true)
+            {
+                selected = false;
+                gm.selectedUnit = null;
+                gm.ResetTiles();
+            }
+            else
+            {
+
+
+                if (gm.selectedUnit != null)
+                {
+                    gm.selectedUnit.selected = false;
+                }
+
+                selected = true;
+                gm.selectedUnit = this;
+
+                gm.ResetTiles();
+                GetWalkableTiles();
+                GetAttackableTiles();
+
+
+            }
+
+        }
+        if (Turn % 2 == 0 && Player == 2)
+        {
+
+
+            if (selected == true)
+            {
+                selected = false;
+                gm.selectedUnit = null;
+                gm.ResetTiles();
+            }
+            else
+            {
+
+
+                if (gm.selectedUnit != null)
+                {
+                    gm.selectedUnit.selected = false;
+                }
+
+                selected = true;
+                gm.selectedUnit = this;
+
+                gm.ResetTiles();
+                GetWalkableTiles();
+                GetAttackableTiles();
+
+            }
+
+        }
+
+    }
+
+    void GetWalkableTiles()
+    {
+
+        if (hasMoved == true) {
+            return;
+        }
+
+        foreach (Tile tile in FindObjectsOfType<Tile>())
+        {
+            if (Mathf.Abs(transform.position.x - tile.transform.position.x) + Mathf.Abs(transform.position.y - tile.transform.position.y) <= tileSpeed)
+            {
+                if (tile.IsClear() == true)
+                {
+                    tile.Highlight();
+                }
+            }
+        }
+
+    }
+    void GetAttackableTiles()
+    {
+
+        if (hasAttacked == true)
+        {
+            return;
+        }
+
+        foreach (Tile tile in FindObjectsOfType<Tile>())
+        {
+            if (Mathf.Abs(transform.position.x - tile.transform.position.x) + Mathf.Abs(transform.position.y - tile.transform.position.y) <= attackRange)
+            {
+                tile.HighlightAttack();
+
+            }
+        }
+
+    }
+
+    public void Move(Transform movePos)
+    {
+        gm.ResetTiles();
+        StartCoroutine(StartMovement(movePos));
+    }
+    
+
+    public void TakeDamage(int Damage)
+    {
+        curHealth -= Damage;
+        if(curHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void isAttackable()
+    {
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
+        
+        if (hitEnemies.Length > 0)
+        {
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                if (enemy.GetComponent<Units>())
+                {
+                    if (enemy.GetComponent<Units>().Player != Player)
+                    {
+                        bool isAttackable = true;
+                        enemy.GetComponent<Units>().attackable = isAttackable;
+                        if (Input.GetKeyDown(KeyCode.A) && hasAttacked == false)
+                        {
+                            enemy.GetComponent<Units>().TakeDamage(attackDamage);
+                            hasAttacked = true;
+                        }
+                    }
+                }
+
+
+            }
+        }
+        else
+            GetComponent<Units>().attackable = false;
+    }
+
+    IEnumerator StartMovement(Transform movePos)
+    {
+
+
+        float startTime = Time.time;
+        float currentTime = Time.time;
+
+        int posx = (int)transform.position.x;
+        int moveposx = (int)movePos.position.x;
+
+        int posy = (int)transform.position.y;
+        int moveposy = (int)movePos.position.y;
+
+
+        Debug.Log(transform.position);
+        Debug.Log(movePos);
+
+        while (posx != moveposx)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(movePos.position.x, transform.position.y), moveSpeed * Time.deltaTime);
+            //Debug.Log(transform.position);
+
+            posx = (int)transform.position.x;
+            moveposx = (int)movePos.position.x;
+            yield return null;
+
+        }
+        transform.position = new Vector3(movePos.position.x, transform.position.y, transform.position.z);
+
+
+
+        while (posy != moveposy)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, movePos.position.y), moveSpeed * Time.deltaTime);
+            //Debug.Log(transform.position);
+
+            posy = (int)transform.position.y;
+            moveposy = (int)movePos.position.y;
+            yield return null;
+
+        }
+        transform.position = new Vector3(transform.position.x, movePos.position.y, transform.position.z);
+
+
+
+        hasMoved = true;
+
+    }
+
+    void Update()
+    {
+        isAttackable();
+
+        EndTurn();
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+    public void EndTurn()
+    {
+     if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Turn += 1;
+            hasMoved = false;
+            hasAttacked = false;
+        }
+    }
+
+
+
+}
